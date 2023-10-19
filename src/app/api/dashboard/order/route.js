27e40c5281ac8319c7ff3a43c142e29e8bd07273
wproman <rmn.tda@gmail.com>
2023-10-19 23:03:20 +0600

    order

--- src/app/api/dashboard/order/route.js
+++ src/app/api/dashboard/order/route.js
@@ -9,10 +9,11 @@ export async function POST(req, res) {
     try {
       const prisma = new PrismaClient();
       let reqBody = await req.json();
-     let result = await prisma.order.create({ data: reqBody });
+      let result = await prisma.order.create({ data: reqBody });
 
       return NextResponse.json({ status: "Success", result: result });
     } catch (err) {
       return NextResponse.json({ status: "Fail", result: err.toString() });
     }
+
 }
