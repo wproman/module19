7605c9b446aaf3d0790e98917494bed3a31ba6ea
wproman <rmn.tda@gmail.com>
2023-10-20 01:03:55 +0600

    product meta

--- src/app/api/dashboard/product/product_meta/route.js
+++ src/app/api/dashboard/product/product_meta/route.js
@@ -9,7 +9,7 @@ export async function POST(req, res) {
     try {
       const prisma = new PrismaClient();
       const reqBody = await req.json();
-      const result = await prisma.productMeta({ data: reqBody });
+      const result = await prisma.productMeta.create({ data: reqBody });
 
       return NextResponse.json({ status: "Success", result: result });
     } catch (err) {
